b44494ca9e3e1aacae2bfdb18f7fe4e8884217b8
will <w251754731@gmail.com>
2025-10-15 17:51:49 +0800

    2.2 字体、颜色、圆角、间隙等样式预设

--- lib/common/style/theme.dart
+++ lib/common/style/theme.dart
@@ -8,6 +8,13 @@ class AppTheme {
   /// 自定义颜色
   /////////////////////////////////////////////////
 
+  // static const primary = Color(0xff865319);
+  // static const secondary = Color(0xff735942);
+  // static const success = Color(0xFF23A757);
+  // static const warning = Color.fromARGB(255, 244, 244, 45);
+  // static const error = Color(0xFFDA1414);
+  // static const info = Color.fromARGB(255, 70, 113, 193);
+
   static const primary = Color(0xFF5F84FF);
   static const secondary = Color(0xFFFF6969);
   static const success = Color(0xFF23A757);
@@ -19,15 +26,43 @@ class AppTheme {
   /// 主题
   /////////////////////////////////////////////////
 
+  /////////////////////////////////////////////////
+  /// 主题
+  /////////////////////////////////////////////////
+
   /// 亮色主题
   static ThemeData get light {
-    ColorScheme scheme = MaterialTheme.lightScheme();
+    ColorScheme scheme = MaterialTheme.lightScheme().copyWith(
+      primary: primary,
+      onPrimary: Colors.white,
+      secondary: secondary,
+      onSecondary: Colors.white,
+      surface: Colors.white,
+      onSurface: const Color(0xFF333333),
+      error: error,
+      onError: Colors.white,
+      //   tertiary: const Color(0xFFF4F6F9),
+      //   outline: const Color(0xFFF4F6F9),
+      //   shadow: const Color(0xFF5A6CEA).withOpacity(0.08),
+    );
     return _getTheme(scheme);
   }
 
   /// 暗色主题
   static ThemeData get dark {
-    ColorScheme scheme = MaterialTheme.darkScheme();
+    ColorScheme scheme = MaterialTheme.darkScheme().copyWith(
+      primary: primary,
+      // onPrimary: Colors.white,
+      secondary: secondary,
+      // onSecondary: Colors.white,
+      // surface: const Color(0xFF252525),
+      // onSurface: Colors.white,
+      error: error,
+      onError: Colors.white,
+      //   tertiary: const Color(0xFF141414),
+      //   outline: const Color(0xFF252525),
+      //   shadow: const Color(0xFF777777).withOpacity(0.08),
+    );
     return _getTheme(scheme);
   }
 
