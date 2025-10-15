c03af39734e91a54ab6e282341167c2ccf10d1c8
will <w251754731@gmail.com>
2025-10-15 17:52:18 +0800

    2.2 字体、颜色、圆角、间隙等样式预设

--- lib/common/style/index.dart
+++ lib/common/style/index.dart
@@ -2,3 +2,9 @@ library;
 
 export 'colors.dart';
 export 'theme.dart';
+export 'border.dart';
+export 'elevation.dart';
+export 'padding.dart';
+export 'radius.dart';
+export 'scale.dart';
+export 'space.dart';
