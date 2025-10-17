4899267d1d86154a002b0867d76c310bbf2ba79e
will <w251754731@gmail.com>
2025-10-17 15:11:06 +0800

    4.1 ScreenUtil 配置

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:device_preview/device_preview.dart';
+import 'package:ducafe_ui_core/ducafe_ui_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -27,33 +28,47 @@ class MyApp extends StatelessWidget {
       dark: AppTheme.dark, // 暗色主题
       initial: ConfigService.to.themeMode, // 初始主题
       debugShowFloatingThemeButton: kDebugMode, // 显示主题按钮, 开发阶段打开, 生产阶段关闭
-      builder: (theme, darkTheme) => GetMaterialApp(
-        title: 'Woo 2025',
-        // 主题
-        // theme: ThemeData(
-        //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        //   useMaterial3: false,
-        // ),
-        theme: theme,
-        darkTheme: darkTheme,
+      builder: (theme, darkTheme) => ScreenUtilInit(
+        designSize: const Size(414, 896), // 设计稿中设备的尺寸(单位随意,建议dp,但在使用过程中必须保持一致)
+        splitScreenMode: false, // 支持分屏尺寸
+        minTextAdapt: false, // 是否根据宽度/高度中的最小值适配文字
+        child: GetMaterialApp(
+          title: 'Woo 2025',
+          // 主题
+          // theme: ThemeData(
+          //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          //   useMaterial3: false,
+          // ),
+          theme: theme,
+          darkTheme: darkTheme,
 
-        // 路由
-        initialRoute: RouteNames.stylesStylesIndex,
-        getPages: RoutePages.list,
-        navigatorObservers: [RoutePages.observer],
+          // 路由
+          initialRoute: RouteNames.stylesStylesIndex,
+          getPages: RoutePages.list,
+          navigatorObservers: [RoutePages.observer],
 
-        // 国际化
-        translations: Translation(), // 词典
-        localizationsDelegates: Translation.localizationsDelegates, // 代理
-        supportedLocales: Translation.supportedLocales, // 支持的语言种类
-        locale: ConfigService.to.locale, // 当前语言种类
-        fallbackLocale: Translation.fallbackLocale, // 默认语言种类
-        // 设备预览
-        // locale: DevicePreview.locale(context),
-        // builder: DevicePreview.appBuilder,
+          // 国际化
+          translations: Translation(), // 词典
+          localizationsDelegates: Translation.localizationsDelegates, // 代理
+          supportedLocales: Translation.supportedLocales, // 支持的语言种类
+          locale: ConfigService.to.locale, // 当前语言种类
+          fallbackLocale: Translation.fallbackLocale, // 默认语言种类
+          // 设备预览
+          // locale: DevicePreview.locale(context),
+          // builder: DevicePreview.appBuilder,
+          builder: (context, widget) {
+            // 不随系统字体缩放比例
+            return MediaQuery(
+              data: MediaQuery.of(
+                context,
+              ).copyWith(textScaler: const TextScaler.linear(1.0)),
+              child: widget!,
+            );
+          },
 
-        // 调试标志
-        debugShowCheckedModeBanner: false,
+          // 调试标志
+          debugShowCheckedModeBanner: false,
+        ),
       ),
     );
   }
