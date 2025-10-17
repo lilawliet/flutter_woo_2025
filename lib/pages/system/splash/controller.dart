6b171ba2eec03113584542f9785916b98840c3ce
will <w251754731@gmail.com>
2025-10-17 16:06:51 +0800

    5.1 WelcomeSlider 封装

--- lib/pages/system/splash/controller.dart
+++ lib/pages/system/splash/controller.dart
@@ -1,9 +1,17 @@
+import 'package:flutter_woo_2025/common/index.dart';
 import 'package:get/get.dart';
 
 class SplashController extends GetxController {
   SplashController();
 
   String title = "";
+
+  _jumpToPage() {
+    // 欢迎页
+    Future.delayed(const Duration(seconds: 1), () {
+      Get.offAllNamed(RouteNames.systemWelcome);
+    });
+  }
 
   _initData() {
     update(["splash"]);
@@ -22,7 +30,8 @@ class SplashController extends GetxController {
   @override
   void onReady() {
     super.onReady();
-    _initData();
+    // _initData(); // 初始数据
+    _jumpToPage(); // 跳转界面
   }
 
   // @override
