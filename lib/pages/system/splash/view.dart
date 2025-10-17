0c3f19767e8bb44e8ddadc36b96690fbd14d6797
will <w251754731@gmail.com>
2025-10-17 15:17:41 +0800

    4.2 启动屏

--- lib/pages/system/splash/view.dart
+++ lib/pages/system/splash/view.dart
@@ -9,7 +9,7 @@ class SplashPage extends GetView<SplashController> {
 
   // 主视图
   Widget _buildView() {
-    return Center(child: Text("SplashPage - ${ConfigService.to.version}"));
+    return const ImageWidget.img(AssetsImages.splashJpg, fit: BoxFit.fill);
   }
 
   @override
@@ -18,10 +18,7 @@ class SplashPage extends GetView<SplashController> {
       init: SplashController(),
       id: "splash",
       builder: (_) {
-        return Scaffold(
-          appBar: AppBar(title: const Text("splash")),
-          body: SafeArea(child: _buildView()),
-        );
+        return _buildView();
       },
     );
   }
