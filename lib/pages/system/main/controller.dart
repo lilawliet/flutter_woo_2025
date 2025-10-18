392b37b43d0447fa835d6482f85aa1aacb9ae202
will <w251754731@gmail.com>
2025-10-18 13:52:33 +0800

    8.1 导航栏编写

--- lib/pages/system/main/controller.dart
+++ lib/pages/system/main/controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_woo_2025/common/index.dart';
 import 'package:get/get.dart';
 
@@ -27,8 +28,52 @@ class MainController extends GetxController {
     // Get.toNamed(RouteNames.systemRegister);
   }
 
-  // @override
-  // void onClose() {
-  //   super.onClose();
-  // }
+  // 分页管理
+  final PageController pageController = PageController();
+
+  // 当前的 tab index
+  int currentIndex = 0;
+
+  // 导航栏切换
+  void onIndexChanged(int index) {
+    currentIndex = index;
+    update(['navigation']);
+  }
+
+  // 切换页面
+  void onJumpToPage(int page) {
+    pageController.jumpToPage(page);
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+    // 释放页控制器
+    pageController.dispose();
+  }
+
+  // 退出请求时间
+  DateTime? currentBackPressTime;
+
+  // 返回键退出
+  bool closeOnConfirm(BuildContext context) {
+    DateTime now = DateTime.now();
+    // 物理键，两次间隔大于4秒, 退出请求无效
+    if (currentBackPressTime == null ||
+        now.difference(currentBackPressTime!) > const Duration(seconds: 4)) {
+      currentBackPressTime = now;
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          behavior: SnackBarBehavior.floating,
+          content: Center(child: Text('Press again to exit the application.')),
+          duration: Duration(seconds: 4),
+        ),
+      );
+      return false;
+    }
+
+    // 退出请求有效
+    currentBackPressTime = null;
+    return true;
+  }
 }
