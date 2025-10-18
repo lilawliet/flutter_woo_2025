579c3703f61c3c1ced57ee49ab5747afe199af02
will <w251754731@gmail.com>
2025-10-18 13:47:24 +0800

    7.4 密码AES加密后传输

--- lib/common/utils/encrypt.dart
+++ lib/common/utils/encrypt.dart
@@ -7,16 +7,29 @@ class EncryptUtil {
   static final EncryptUtil _instance = EncryptUtil._internal();
   factory EncryptUtil() => _instance;
   EncryptUtil._internal() {
-    encrypter = Encrypter(AES(key, mode: AESMode.cbc, padding: 'PKCS7'));
+    // 加密器
+    encrypter = Encrypter(
+      AES(
+        key, // 密钥
+        mode: AESMode.cbc, // 模式
+        padding: 'PKCS7', // 填充模式
+      ),
+    );
   }
 
+  /// 密钥
   final key = Key.fromUtf8(Constants.aesKey);
+
+  /// 偏移量
   final iv = IV.fromUtf8(Constants.aesIV);
+
   late Encrypter encrypter;
 
   /// aes加密
   String aesEncode(String content) {
+    // 加密
     final encrypted = encrypter.encrypt(content, iv: iv);
+    // 返回 base64 编码
     return encrypted.base64;
   }
 }
