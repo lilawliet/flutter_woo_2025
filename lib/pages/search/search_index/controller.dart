71eabe57a82cbeb95c1781fdbc4ef24e9eaa7b7a
will <w251754731@gmail.com>
2025-10-19 17:24:51 +0800

    13.1 搜索栏防抖处理

--- lib/pages/search/search_index/controller.dart
+++ lib/pages/search/search_index/controller.dart
@@ -1,11 +1,47 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class SearchIndexController extends GetxController {
   SearchIndexController();
 
-  _initData() {
-    update(["search_index"]);
+  // 搜索控制器
+  final TextEditingController searchEditController = TextEditingController();
+
+  // 搜索关键词
+  final searchKeyWord = "".obs;
+
+  // 搜索栏位 - 防抖
+  void searchDebounce() {
+    // getx 内置防抖处理
+    debounce(
+      // obs 对象
+      searchKeyWord,
+
+      // 回调函数
+      (value) async {
+        // 调试
+        if (kDebugMode) {
+          print("debounce -> $value");
+        }
+
+        // 拉取数据
+        update(["search_index"]);
+      },
+
+      // 延迟 500 毫秒
+      time: const Duration(milliseconds: 500),
+    );
+
+    // 监听搜索框变化
+    searchEditController.addListener(() {
+      searchKeyWord.value = searchEditController.text;
+    });
   }
+
+  // _initData() {
+  //   update(["search_index"]);
+  // }
 
   void onTap() {}
 
@@ -17,11 +53,14 @@ class SearchIndexController extends GetxController {
   @override
   void onReady() {
     super.onReady();
-    _initData();
+    // 注册防抖
+    searchDebounce();
   }
 
-  // @override
-  // void onClose() {
-  //   super.onClose();
-  // }
+  @override
+  void onClose() {
+    super.onClose();
+    // 搜索控制器释放
+    searchEditController.dispose();
+  }
 }
--- lib/pages/search/search_index/view.dart
+++ lib/pages/search/search_index/view.dart
@@ -1,10 +1,28 @@
+import 'package:ducafe_ui_core/ducafe_ui_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_woo_2025/common/index.dart';
 import 'package:get/get.dart';
 
 import 'index.dart';
 
 class SearchIndexPage extends GetView<SearchIndexController> {
   const SearchIndexPage({super.key});
+
+  // 导航栏
+  AppBar _buildAppBar() {
+    return AppBar(
+      titleSpacing: AppSpace.listItem,
+      // 搜索栏
+      title: InputWidget(
+        controller: controller.searchEditController,
+        placeholder: "You can try T-Shirt", //LocaleKeys.commonSearchInput.tr,
+        prefix: const IconWidget.icon(Icons.search),
+        // onChanged: (val) {
+        //   printInfo(info: val);
+        // },
+      ).paddingRight(AppSpace.page),
+    );
+  }
 
   // 主视图
   Widget _buildView() {
@@ -18,7 +36,10 @@ class SearchIndexPage extends GetView<SearchIndexController> {
       id: "search_index",
       builder: (_) {
         return Scaffold(
-          appBar: AppBar(title: const Text("search_index")),
+          // 搜索栏
+          appBar: _buildAppBar(),
+
+          // 内容
           body: SafeArea(child: _buildView()),
         );
       },
