70b86fc53e4fcc37672876f9ce38a31f8cacaf68
will <w251754731@gmail.com>
2025-10-20 18:17:50 +0800

    17.2 购物车 UI 编写

--- lib/pages/cart/cart_index/view.dart
+++ lib/pages/cart/cart_index/view.dart
@@ -1,4 +1,6 @@
+import 'package:ducafe_ui_core/ducafe_ui_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_woo_2025/common/index.dart';
 import 'package:get/get.dart';
 
 import 'index.dart';
@@ -6,20 +8,94 @@ import 'index.dart';
 class CartIndexPage extends GetView<CartIndexController> {
   const CartIndexPage({super.key});
 
+  // 订单列表
+  Widget _buildOrders() {
+    return ListView.separated(
+      itemBuilder: (BuildContext context, int index) {
+        LineItem item = CartService.to.lineItems[index];
+        return Text(item.product?.name ?? "").paddingAll(AppSpace.card).card();
+      },
+      separatorBuilder: (BuildContext context, int index) {
+        return SizedBox(height: AppSpace.listRow);
+      },
+      itemCount: CartService.to.lineItems.length,
+    );
+  }
+
+  // 优惠券, 568935ab
+  Widget _buildCoupons() {
+    return <Widget>[
+      // 输入框
+      const InputWidget(placeholder: "Voucher Code").expanded(),
+
+      // 应用按钮
+      ButtonWidget.ghost(LocaleKeys.gCartBtnApplyCode.tr),
+    ].toRow();
+  }
+
+  // 统计
+  Widget _buildTotal(BuildContext context) {
+    return <Widget>[
+          // 运费、代金券
+          <Widget>[
+                // 运费
+                TextWidget.label(
+                  "${LocaleKeys.gCartTextShippingCost.tr}: \$${CartService.to.shipping}",
+                ),
+                // 代金券
+                TextWidget.label("${LocaleKeys.gCartTextVocher.tr}: "),
+              ]
+              .toColumn(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+              )
+              .expanded(),
+
+          // 费用小计
+          TextWidget.label(
+            "${LocaleKeys.gCartTextTotal.tr}: \$${CartService.to.totalItemsPrice + CartService.to.shipping}",
+          ).paddingRight(AppSpace.iconTextMedium),
+
+          // 确认下单 checkout
+          ButtonWidget.primary(
+            LocaleKeys.gCartBtnCheckout.tr,
+            onTap: () {},
+            // borderRadius: 3.sp,
+          ),
+        ]
+        .toRow()
+        .paddingAll(AppSpace.card)
+        .decorated(
+          color: context.colors.scheme.secondary.withOpacity(0.1),
+          border: Border.all(color: context.colors.scheme.secondary, width: 1),
+        );
+  }
+
   // 主视图
-  Widget _buildView() {
-    return const Center(child: Text("CartIndexPage"));
+  Widget _buildView(BuildContext context) {
+    return <Widget>[
+      // 顶部操作栏
+
+      // 订单列表
+      _buildOrders().paddingHorizontal(AppSpace.page).expanded(),
+
+      // 优惠券
+      _buildCoupons(),
+
+      // // 费用小计
+      _buildTotal(context),
+    ].toColumn();
   }
 
   @override
   Widget build(BuildContext context) {
     return GetBuilder<CartIndexController>(
-      init: CartIndexController(),
+      init: Get.find<CartIndexController>(),
       id: "cart_index",
       builder: (_) {
         return Scaffold(
-          appBar: AppBar(title: const Text("cart_index")),
-          body: SafeArea(child: _buildView()),
+          appBar: mainAppBarWidget(titleString: LocaleKeys.gCartTitle.tr),
+          body: SafeArea(child: _buildView(context)),
         );
       },
     );
